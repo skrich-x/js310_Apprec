a23e6b8d017910d1cd1376ec6bc39b6d23da7e87
Kyle Richardson <kylerichardson@Kyles-MacBook-Pro-2.local>
2015-11-12 07:29:39 -0500

    additional styling and function for parse

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+(function(){
+  /*global -$ */
+  'use strict';
+
+  var gulp = require('gulp');
+  var $ = require('gulp-load-plugins')();
+  var browserSync = require('browser-sync');
+  var reload = browserSync.reload;
+
+  gulp.task('styles', function () {
+    return gulp.src('styles/main.scss')
+      .pipe($.sourcemaps.init())
+      .pipe($.sass({
+        outputStyle: 'nested', // libsass doesn't support expanded yet
+        precision: 10,
+        includePaths: ['.'],
+        onError: console.error.bind(console, 'Sass error:')
+      }))
+      .pipe($.postcss([
+        require('autoprefixer-core')({browsers: ['last 1 version']})
+      ]))
+      .pipe($.sourcemaps.write())
+      .pipe(gulp.dest('dist/styles'))
+      .pipe(reload({stream: true}));
+  });
+
+  gulp.task('scripts', function () {
+    return gulp.src('scripts/**/*.js')
+      .pipe($.sourcemaps.init())
+      .pipe($.plumber())
+      .pipe($.babel())
+      .pipe($.concat('main.js'))
+      .pipe($.sourcemaps.write('.'))
+      .pipe(gulp.dest('dist/scripts/'))
+      .pipe(reload({stream: true}));
+  });
+
+  gulp.task('build', ['scripts', 'styles'], function(){});
+
+  gulp.task('serve', ['build'], function () {
+    browserSync({
+      notify: false,
+      port: 9000,
+      server: {
+        baseDir: ['./']
+      }
+    });
+
+    // watch for changes
+    gulp.watch([
+      '*.html',
+      'scripts/**/*.js'
+    ]).on('change', reload);
+
+    gulp.watch('styles/**/*.scss', ['styles']);
+    gulp.watch('scripts/**/*.js', ['scripts']);
+  });
+
+  gulp.task('default', ['serve'], function (){});
+})();
